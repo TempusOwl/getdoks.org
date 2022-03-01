9e3a8641c17adbd457345ac1ae1b6c8c5539e002
TempusOwl <Olympus@binarychronos.com>
2022-03-01 16:07:42 -0500

    Debug On

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -11,5 +11,5 @@ docsearch({
   appId: '6YP209XB51',
   indexName: 'netlify_d836a4f4-95a8-4205-8592-79dd339bee3e_master_all',
   apiKey: '5e9e7a5a9ae79dcf00c4762f73c01823',
-  debug: false,
+  debug: true,
 });
